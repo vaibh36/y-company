36ef8042294140070afa21e9c4949f6d1b0fd118
Vaibhav Gera <vaibhav.gera@nagarro.com>
2024-06-11 10:35:53 +0530

    chore: added few more test cases for the components and pages

---
+++ src/pages/FilterProducts/FilterProducts.test.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { renderWithProviders } from "../../testUtils";
+import FilterProducts from "./index";
+import { MemoryRouter } from "react-router-dom";
+import { MyProductProvider } from "../../context/product-provide";
+
+jest.mock("firebase/firestore", () => {
+  return {
+    getFirestore: jest.fn(),
+    collection: jest.fn(),
+    getDocs: async () => {
+      return [
+        {
+          data: () => {
+            return {
+              id: "1212",
+              price: 12,
+              image:
+                "https://ace-clothing.netlify.app/assets/home-photo-1-257jNod0.webp",
+              description: "12121",
+              stripeId: "21212",
+            };
+          },
+        },
+      ];
+    },
+  };
+});
+
+test("basic test case", () => {
+  renderWithProviders(
+    <MemoryRouter>
+      <MyProductProvider>
+        <FilterProducts />
+      </MyProductProvider>
+    </MemoryRouter>
+  );
+});
